51c4c0a991b4bf919f7e5779c06f3ed87ba8535a
Armin19 <rmeen.shokati@gmail.com>
2017-07-01 23:08:03 +0430

    first commit

---
+++ src/net/shokati/Main.java
@@ -0,0 +1,22 @@
+package net.shokati;
+
+
+public class Main {
+    private static int a;
+    private static int b;
+
+    public static void main(String[] args) {
+        System.out.println(addition(3, 4));
+    }
+
+    public static int addition(int a,int b){
+       int r;
+       r = a + b;
+    return r;
+
+    }
+}
+
+
+
+
---
+++ src/net/shokati/MainTest.groovy
@@ -0,0 +1,7 @@
+package net.shokati
+
+/**
+ * Created by Armin on 2/19/2017.
+ */
+class MainTest extends groovy.util.GroovyTestCase {
+}
